1a298e72c1fbd7b561a4e25ab392f8310448ebb2
Scolo-WebDesign <120602601+Scolo-WebDesign@users.noreply.github.com>
2022-12-14 22:18:39 +0100

    Add files via upload
    THIM-IT

---
+++ javaS/code.js
@@ -0,0 +1,23 @@
+
+
+let btn = document.querySelector('#btn');
+let img1 = document.getElementById('img1');
+
+
+btn.addEventListener ('click',  ( ) => {
+     img1.classList.toggle('show');
+
+console.log('yes')
+
+});
+
+
+
+
+
+
+
+
+
+
+
